a2d5feeab55483e0b9feb82e0da3350f81a1f2f7
Heiko Damaske <heiko.damaske@code.berlin>
2018-09-25 18:09:24 +0000

    cash is also working with spegatti spaghetti code

--- pset1/cash/cash.c
+++ pset1/cash/cash.c
@@ -9,6 +9,7 @@ int main(void){
     } while(userchange < 0);
 
     userchange = userchange * 100;
+    int newuserchange = (int) userchange;
 
     int coincount = 0;
 
@@ -17,15 +18,39 @@ int main(void){
     int nickel = 5;
     int penny = 1;
 
-    int countCoinFit (int amount){
-        printf('%i', amount);
-        // while(newamount >= coin){
-        //     count = newamount % coin;
-        //     newamount = newamount - (coin*count);
-        // }
+    int rest = 0;
+    if(newuserchange >= quarter){
+        rest = newuserchange%quarter;
+        coincount += (newuserchange - rest)/quarter;
+        newuserchange = newuserchange - (((newuserchange - rest)/quarter)*quarter);
+    }
+    if(newuserchange >= dime){
+        rest = newuserchange%dime;
+        coincount += (newuserchange - rest)/dime;
+        newuserchange = newuserchange - (((newuserchange - rest)/dime)*dime);
+    }
+    if(newuserchange >= nickel){
+        rest = newuserchange%nickel;
+        coincount += (newuserchange - rest)/nickel;
+        newuserchange = newuserchange - (((newuserchange - rest)/nickel)*nickel);
+    }
+    if(newuserchange >= penny){
+        rest = newuserchange%penny;
+        coincount += (newuserchange - rest)/penny;
+        newuserchange = newuserchange - (((newuserchange - rest)/penny)*penny);
     }
 
-    countCoinFit(quarter);
+    printf("%i\n", coincount);
+
+    // int countCoinFit (int amount){
+    //     printf('%i', amount);
+    //     // while(newamount >= coin){
+    //     //     count = newamount % coin;
+    //     //     newamount = newamount - (coin*count);
+    //     // }
+    // }
+
+    // countCoinFit(quarter);
 
 
 }
