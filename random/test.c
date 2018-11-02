d1583859cbed87051490703d59d5d6cf43e3276b
Heiko Damaske <heiko.damaske@code.berlin>
2018-11-02 09:50:27 +0000

    helpers done

---
+++ random/test.c
@@ -0,0 +1,12 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+int main(void){
+    char c = 'a';
+    int n = (int) c;
+    printf("%i\n", n);
+
+}
